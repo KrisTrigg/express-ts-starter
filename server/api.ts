4fadf004ddd3cc78ee41649cb11ad44dbb2c817c
Kris <kristrigg@hotmail.co.uk>
2017-10-08 01:05:56 +0100

    Not found routing

--- server/api.ts
+++ server/api.ts
@@ -2,4 +2,6 @@ import * as express from 'express';
 import * as user from './user-router';
 export var router = express.Router();
 router.use('/user', user.router);
-
+router.use((req, res) => {
+  res.status(404).send("Not Found");
+})
--- server/app.ts
+++ server/app.ts
@@ -19,6 +19,9 @@ app.use('/api', api.router);
 //static file routing
 app.use(express.static(path.join(__dirname, '../client')));
 
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '../client/404.html'));
+})
 //start server
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
